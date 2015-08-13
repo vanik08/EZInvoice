5553dde66d990f11312cb200d99830a8e9135cc4
vanik08 <vanik08@gmail.com>
2015-08-12 19:53:10 -0700

    Tables created for each invoice and inputs

--- client/app/invoice/invoice.controller.js
+++ client/app/invoice/invoice.controller.js
@@ -12,22 +12,42 @@
 		.module('ezinvoiceApp')
 		.controller('InvoiceCtrl', InvoiceCtrl)
 
-	function InvoiceCtrl(Ajax, $stateParams, $scope) {
+	function InvoiceCtrl(Ajax, $stateParams, $scope, $modal) {
 		//jshint validthis=true
 		var vm = this;
 		vm.invoice = $scope.$parent.currentInvoice;
+    vm.loadInvoice = loadInvoice;
 
-		//When URL is not re-directed and the parent scope is empty,
-		//such as the page being refreshed, request and find the invoice
-		if(vm.invoice == undefined) {
-			console.warn('undefined');
-			Ajax.getInvoices(function (data) {
-				data.forEach(function (item) {
-					if(item._id === $stateParams.id) {
-						vm.invoice = item;
-					}
-				})
-			});
-		}
+    vm.inputJob;
+    vm.inputHours;
+    vm.inputTimeStarted;
+    vm.inputTimeEnded;
+    vm.inputNotes;
+    vm.inputRate;
+    vm.inputTax;
+    vm.inputTotal;
+
+    $scope.$watch('vm.inputRate', function (current) {
+      vm.inputTotal = current || 0;
+      console.log('watched');
+      debugger;
+    });
+
+    vm.loadInvoice();
+
+    function loadInvoice() {
+  		//When URL is not re-directed and the parent scope is empty,
+  		//such as the page being refreshed, request and find the invoice
+  		if(vm.invoice == undefined) {
+  			console.warn('undefined');
+  			Ajax.getInvoices(function (data) {
+  				data.forEach(function (item) {
+  					if(item._id === $stateParams.id) {
+  						vm.invoice = item;
+  					}
+  				})
+  			});
+  		}
+    }
 	}
 })();
--- server/api/entry/entry.model.js
+++ server/api/entry/entry.model.js
@@ -12,7 +12,6 @@ var EntrySchema = new Schema({
   endTime: Date,
   total: Number,
   tax: Number,
-  date: Date,
   notes: String
 });
 
